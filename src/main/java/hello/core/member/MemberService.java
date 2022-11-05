56e6774d6eb033cf2db79f63e2ee1a5802015f91
mitsui3point <rladbsgh0331@gmail.com>
2022-11-05 09:43:30 +0900

    style(OrderService, MemberService): add comments

--- src/main/java/hello/core/member/MemberService.java
+++ src/main/java/hello/core/member/MemberService.java
@@ -1,7 +1,16 @@
 package hello.core.member;
 
 public interface MemberService {
+    /**
+     * 회원 가입
+     * @param member
+     */
     void join(Member member);
 
+    /**
+     * 회원 단건 조회
+     * @param id
+     * @return 회원 단건
+     */
     Member findMember(Long id);
 }
--- src/main/java/hello/core/order/OrderService.java
+++ src/main/java/hello/core/order/OrderService.java
@@ -1,5 +1,12 @@
 package hello.core.order;
 
 public interface OrderService {
+    /**
+     * 주문 단건 생성
+     * @param memberId
+     * @param itemName
+     * @param itemPrice
+     * @return 주문 단건
+     */
     Order createOrder(Long memberId, String itemName, int itemPrice);
 }
