1b7a2d980ffa68a81a12bb27ff381eb41256041e
mitsui3point <rladbsgh0331@gmail.com>
2022-11-07 23:30:15 +0900

    feat(SingletonService): add
    SingletonService
    - add SingletonService private constructor
    - add getInstance, logic methods
    - add private instance field
    
    feat(SingletonTest): update
    
    SingletonTest
    - add singletonServiceTest test method

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -23,4 +23,23 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        //given
+        //private 으로 생성자를 막아두었다. 컴파일 오류가 발생.
+        //new SingletonService();
+
+        //when
+        SingletonService singletonService1 = SingletonService.getInstance(); // 1. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance(); // 2. 조회: 호출할 때 마다 같은 객체를 반환
+
+        //then
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+        assertThat(singletonService1).isSameAs(singletonService2); // singletonService1 == singletonService2; 참조가 같은 경우, equal; value 가 같은 경우
+
+        singletonService1.logic();
+    }
 }
