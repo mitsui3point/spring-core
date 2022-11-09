cccd31ed2c17fc2aa8c53a7155205a8671ed845b
mitsui3point <rladbsgh0331@gmail.com>
2022-11-09 16:58:54 +0900

    feat(OrderServiceImpl, MemberServiceImpl): update for spring log message (Overriding bean definition for bean 'memoryMemberRepository')
    AutoAppConfig
    - add memberRepository method

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,19 +1,26 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+
+import static org.springframework.context.annotation.ComponentScan.Filter;
 
 @Configuration
 @ComponentScan(
 //        basePackages = "hello.core.member",
 //        basePackageClasses = MemberService.class,
-        excludeFilters = @ComponentScan.Filter(
-            type = FilterType.ANNOTATION,
-            classes = Configuration.class)
+        excludeFilters = @Filter(
+                type = FilterType.ANNOTATION,
+                classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    @Bean(value = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
